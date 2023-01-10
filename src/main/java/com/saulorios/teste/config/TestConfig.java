a71622de91887847675a18bec7cd3dc0ca5badfc
Saulo_Rios <40133707+saulorios@users.noreply.github.com>
2023-01-10 00:49:01 -0300

    Payment, one-to-one association

--- src/main/java/com/saulorios/teste/config/TestConfig.java
+++ src/main/java/com/saulorios/teste/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.saulorios.teste.entities.Category;
 import com.saulorios.teste.entities.Order;
 import com.saulorios.teste.entities.OrderItem;
+import com.saulorios.teste.entities.Payment;
 import com.saulorios.teste.entities.Product;
 import com.saulorios.teste.entities.User;
 import com.saulorios.teste.entities.enums.OrderStatus;
@@ -81,5 +82,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	} 
 }
